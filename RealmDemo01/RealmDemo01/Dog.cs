965422088587039af3c31e3deb17d6b49bd89af6
yoshzawa <t.yoshizawa@jc-21.jp>
2017-11-29 11:56:00 +0900

    FodyWeavers.xmlを直したり、Dogクラスを追加したり

---
+++ RealmDemo01/RealmDemo01/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Realms;
+
+namespace RealmDemo01
+{
+    public class Dog : RealmObject
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person Owner { get; set; }
+    }
+}
